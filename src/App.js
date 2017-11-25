640747985188ab87ab26b0a324a7b5f407513059
GoodWorkLabs <GoodWork@GoodWorkLabss-MacBook-Pro.local>
2017-11-25 14:08:02 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import CustomComponent from 'components/CustomComponent'
+class App extends React.Component {
+  render() {
+    return ( <CustomComponent >
+          1
+      </CustomComponent>
+    )
+  }
+}
+export default App
